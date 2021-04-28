7217556f6cb9bdf3185828a2bed829e9ef2f7b45
jolbrys <jakub.olbrys@gmail.com>
2021-04-28 20:59:53 +0200

    tresc drugiego wariantu do zadania ze statikow

--- src/com/sda/oop/statiki/Kalkulator.java
+++ src/com/sda/oop/statiki/Kalkulator.java
@@ -14,8 +14,14 @@ public class Kalkulator {
 // mnozenie, dzielenie reszta z dzielenia (%)
 // robimy zmienną statyczna ktora bedzie przechowywac znak dzialania
 //w mainie: zczytujemy string ze znakiem i nadpisujemy nim zmienna w klasie Kalkulator
-//w mainie: switch case lub ify, w zależości od znaku (który pobieramyz klasy)
+//w mainie lub Kalkulatorze: switch case lub ify, w zależości od znaku (który pobieramyz klasy)
 //wolamy odpowiednie metody z klasy kalkulator
 
-// 2 wariant: robimy prywatne metody do dzialan w kalkulatorze i jedna statyczna
-// która ma w sobie ify i wywołuje odpowiednia metode
+/*
+ 2 wariant:
+ - w klasie Kalkulator, przerabiamy metody z dzialaniami arytmetycznymi na prywatne
+ - rowniez w klasie Kalkulator, dodajemy publiczna metode ktora w zaleznosci od znaku wykonuje odpowiednie dzialanie
+   wlasciwie chodzi o to zeby przerzucic tam ten switch-case ktory jest w mainie
+ - w mainie zostaje pobranie liczb i znaku, przypisane znaku do zmienniej statycznej i wywolanie metody publicznej
+
+*/
